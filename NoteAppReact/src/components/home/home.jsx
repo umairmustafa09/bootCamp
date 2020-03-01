ce10ba8a5eed2f194ff9ad278789f920ecbe6676
Umair Mustafa <umairhassaaan@gmail.com>
2020-03-01 09:00:48 +0500

    updated

--- NoteAppReact/src/components/home/home.jsx
+++ NoteAppReact/src/components/home/home.jsx
@@ -3,89 +3,141 @@ import { Link } from 'react-router-dom';
 import './home.css';
 
 export default class Home extends Component {
-  state = {
-    notes: JSON.parse(localStorage.getItem('Notes')),
-    sortedbyCreated: [],
-    sortedbyAlphabet: []
-  };
+	constructor(props) {
+		super(props);
+	}
 
-  remove = index => {
-    console.log(index);
-    this.state.notes.splice(index, 1);
-    window.localStorage.setItem('Notes', JSON.stringify(this.state.notes));
-    window.location.reload();
-  };
+	state = {
+		notes:
+			JSON.parse(localStorage.getItem('Notes')) == null
+				? []
+				: JSON.parse(localStorage.getItem('Notes')),
+		sortedbyCreated: [],
+		sortedbyAlphabet: [],
+		searched: []
+	};
 
-  edit = index => {
-    let value = window.prompt('Enter Changing in the body');
-    this.state.notes[index].body = value;
-    window.localStorage.setItem('Notes', JSON.stringify(this.state.notes));
-    window.location.reload();
-  };
+	remove = (index) => {
+		this.state.notes.splice(index, 1);
+		window.localStorage.setItem('Notes', JSON.stringify(this.state.notes));
+		window.location.reload();
+	};
 
-  sortByCreated = () => {
-    this.state.sortedbyCreated = this.state.notes.sort();
-    console.log(this.state.sortedbyCreated);
-  };
+	edit = (index) => {
+		let value = window.prompt('Enter Changing in the body');
+		this.state.notes[index].body = value;
+		window.localStorage.setItem('Notes', JSON.stringify(this.state.notes));
+		window.location.reload();
+	};
 
-  sortByAlphabet = () => {
-    this.state.sortedbyAlphabet = this.state.notes;
+	sortByCreated = () => {
+		this.state.sortedbyCreated = this.state.notes.sort();
+		console.table(this.state.sortedbyCreated);
+	};
 
-    for (let i = 0; i < this.state.sortedbyAlphabet; i++) {
-      for (let j = i; j < this.state.sortedbyAlphabet - i - 1; j++) {
-        if (
-          this.state.sortedbyAlphabet[j].title >
-          this.state.sortedbyAlphabet[j + 1].title
-        ) {
-          const temp = this.state.sortedbyAlphabet[j];
-          this.state.sortedbyAlphabet[j] = this.state.sortedbyAlphabet[j + 1];
-          this.state.sortedbyAlphabet[j + 1] = temp;
-        }
-      }
-    }
-    console.log(this.state.sortedbyAlphabet);
-  };
+	sortByAlphabet = () => {
+		this.state.sortedbyAlphabet = this.state.notes;
 
-  render() {
-    return (
-      <div>
-        <div className="noteContainer">
-          {this.state.notes.map((note, i) => (
-            <div id={i}>
-              <p>
-                {note.title} <br />
-                {note.body}
-                <br />
-                <button
-                  onClick={() => {
-                    this.edit(i);
-                  }}
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => {
-                    this.remove(i);
-                  }}
-                >
-                  Delete
-                </button>
-              </p>
-            </div>
-          ))}
-        </div>
-        <div className="container">
-          <Link to="/note">
-            <button>Create Note</button>
-          </Link>
-        </div>
-        <button className="btn" onClick={this.sortByCreated}>
-          sortByCreated
-        </button>
-        <button className="btn" onClick={this.sortByAlphabet}>
-          sortByAlphabet
-        </button>
-      </div>
-    );
-  }
+		for (let i = 0; i < this.state.sortedbyAlphabet; i++) {
+			for (let j = i; j < this.state.sortedbyAlphabet - i - 1; j++) {
+				if (
+					this.state.sortedbyAlphabet[j].title >
+					this.state.sortedbyAlphabet[j + 1].title
+				) {
+					const temp = this.state.sortedbyAlphabet[j];
+					this.state.sortedbyAlphabet[j] = this.state.sortedbyAlphabet[j + 1];
+					this.state.sortedbyAlphabet[j + 1] = temp;
+				}
+			}
+		}
+		console.table(this.state.sortedbyAlphabet);
+	};
+
+	search = (e) => {
+		const notes = this.state.notes.filter(
+			(note) => note.title == e.target.value
+		);
+		this.setState({ searched: notes });
+	};
+
+	displaySearch = () => {
+		return this.state.searched.map((note, i) => {
+			return (
+				<div>
+					<p>
+						{note.title}
+						<br />
+						{note.body}
+						<br />
+						<button
+							onClick={() => {
+								this.edit(i);
+							}}
+						>
+							Edit
+						</button>
+						<button
+							onClick={() => {
+								this.remove(i);
+							}}
+						>
+							Delete
+						</button>
+					</p>
+				</div>
+			);
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="noteContainer">
+					{this.state.notes.map((note, i) => (
+						<div id={i}>
+							<p>
+								{note.title} <br />
+								{note.body}
+								<br />
+								<button
+									onClick={() => {
+										this.edit(i);
+									}}
+								>
+									Edit
+								</button>
+								<button
+									onClick={() => {
+										this.remove(i);
+									}}
+								>
+									Delete
+								</button>
+							</p>
+						</div>
+					))}
+				</div>
+				<div className="container">
+					<Link to="/note">
+						<button>Create Note</button>
+					</Link>
+				</div>
+				<button className="btn" onClick={this.sortByCreated}>
+					sortByCreated
+				</button>
+				<button className="btn" onClick={this.sortByAlphabet}>
+					sortByAlphabet
+				</button>
+				<div className="container">
+					<input
+						type="text"
+						placeholder="Type title to search"
+						id="input"
+						onChange={this.search}
+					/>
+				</div>
+				<div className="noteContainer">{this.displaySearch()}</div>
+			</div>
+		);
+	}
 }
--- NoteAppReact/src/components/note/note.jsx
+++ NoteAppReact/src/components/note/note.jsx
@@ -3,46 +3,49 @@ import { Link } from 'react-router-dom';
 import './note.css';
 
 export default class Note extends Component {
-  state = {
-    noteData: JSON.parse(localStorage.getItem('Notes'))
-  };
+	state = {
+		noteData:
+			JSON.parse(localStorage.getItem('Notes')) == null
+				? []
+				: JSON.parse(localStorage.getItem('Notes'))
+	};
 
-  inputData = () => {
-    const title = document.getElementById('title').value;
-    const body = document.getElementById('body').value;
-    const note = {
-      title: title,
-      body: body,
-      Time: new Date()
-    };
-    this.state.noteData.push(note);
-    window.localStorage.setItem('Notes', JSON.stringify(this.state.noteData));
-    alert('Note is created');
-  };
+	inputData = () => {
+		const title = document.getElementById('title').value;
+		const body = document.getElementById('body').value;
+		const note = {
+			title: title,
+			body: body,
+			Time: new Date()
+		};
+		this.state.noteData.push(note);
+		window.localStorage.setItem('Notes', JSON.stringify(this.state.noteData));
+		alert('Note is created');
+	};
 
-  render() {
-    return (
-      <div className="logIn">
-        <h1>Create Note</h1>
-        <input
-          type="text"
-          name="title"
-          placeholder="Enter a title"
-          id="title"
-        />
-        <input
-          type="text"
-          name="body"
-          placeholder="Enter a body text"
-          id="body"
-        />
-        <button onClick={this.inputData} className="button">
-          Create Note
-        </button>
-        <Link to="/">
-          <button className="button">Go Back</button>
-        </Link>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="logIn">
+				<h1>Create Note</h1>
+				<input
+					type="text"
+					name="title"
+					placeholder="Enter a title"
+					id="title"
+				/>
+				<input
+					type="text"
+					name="body"
+					placeholder="Enter a body text"
+					id="body"
+				/>
+				<button onClick={this.inputData} className="button">
+					Create Note
+				</button>
+				<Link to="/">
+					<button className="button">Go Back</button>
+				</Link>
+			</div>
+		);
+	}
 }
